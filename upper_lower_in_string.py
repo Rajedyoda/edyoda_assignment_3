ea2307b8c07560c771b1eeaa9673c786d7f6161b
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2022-08-01 11:14:12 +0530

    upper_lower_calc_string
    function that accepts a string and calculate the number of upper case letters and lower case letters

---
+++ upper_lower_in_string.py
@@ -0,0 +1,15 @@
+def string_given(str1):
+   str1 = input("Enter a string to calculate no of upper and lower case: ")
+   data={"UPPER_CASE":0, "LOWER_CASE":0}
+   for char in str1:
+      if char.isupper():
+           data["UPPER_CASE"]+=1
+      elif char.islower():
+           data["LOWER_CASE"]+=1
+      else:
+           pass
+   print ("Original String : ", str1)
+   print ("No. of Upper case characters : ", data["UPPER_CASE"])
+   print ("No. of Lower case Characters : ", data["LOWER_CASE"])
+
+string_given('data')
